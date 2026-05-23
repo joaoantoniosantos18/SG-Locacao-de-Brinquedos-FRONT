a6af2806bbd643b0da013d2e366d3c1802c1792c
joaoantoniosantos18 <joaoantoniomacedo1018@gmail.com>
2026-05-23 02:18:14 -0300

    Atualizando a main e o app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,53 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import RotaProtegida from './components/RotaProtegida'
+
+import Login from './pages/Login'
+import Cadastro from './pages/Cadastro'
+import Home from './pages/Home'
+import FazerAgendamento from './pages/FazerAgendamento'
+import MeusAgendamentos from './pages/MeusAgendamentos'
+import AdminPainel from './pages/AdminPainel'
+import FuncionarioPainel from './pages/FuncionarioPainel'
+
 function App() {
-  return <div>funcionando</div>
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+
+      <Route path="/" element={
+        <RotaProtegida roles={['cliente']}>
+          <Home />
+        </RotaProtegida>
+      } />
+
+      <Route path="/agendar" element={
+        <RotaProtegida roles={['cliente']}>
+          <FazerAgendamento />
+        </RotaProtegida>
+      } />
+
+      <Route path="/meus-agendamentos" element={
+        <RotaProtegida roles={['cliente']}>
+          <MeusAgendamentos />
+        </RotaProtegida>
+      } />
+
+      <Route path="/admin" element={
+        <RotaProtegida roles={['admin']}>
+          <AdminPainel />
+        </RotaProtegida>
+      } />
+
+      <Route path="/funcionario" element={
+        <RotaProtegida roles={['funcionario']}>
+          <FuncionarioPainel />
+        </RotaProtegida>
+      } />
+
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  )
 }
 
 export default App
